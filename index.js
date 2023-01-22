8cdc91c0f08e76d991c15d7fdc5420580c094953
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-21 19:02:34 -0700

    Banking app

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
-let balance = 500.00;
+const balance = 500.00
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor (username) {
+    this.username = username
+    // this.balance = 0;
+    this.transactions = []
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance () {
+    let sum = 0
+    this.transactions.map(value => { sum += value })
+    return sum
   }
 
+  addTransaction (value) { this.transactions.push(value) }
+}
+class Transaction {
+  constructor (amount, account) {
+    this.amount = amount
+    this.account = account
+  }
+
+  commit () {
+    this.time = new Date()
+    this.account.addTransaction(this.value())
+    return 'Deposit Succesfull'
+  }
 }
 
+class Withdrawal extends Transaction {
+  value () {
+    return -this.amount
+  }
 
+  commit () {
+    if ((this.account.balance - this.amount) < 0) {
+      return '0'
+    } else { super.commit() }
+  }
+}
 
+class Deposit extends Transaction {
+  value () {
+    return this.amount
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account()
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Starting Account Balance: ', myAccount.balance)
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log('withdrawal is not allowed')
+const t1 = new Withdrawal(13.00, myAccount)
+console.log('Commit result:', t1.commit())
+console.log('Account Balance: ', myAccount.balance)
 
-console.log('Balance:', balance);
+console.log('Withdrawal attempted and Failed! Only Depositing is allowed')
+const t2 = new Deposit(99.99, myAccount)
+console.log('Commit result:', t2.commit())
+console.log('Account Balance: ', myAccount.balance)
+
+console.log('Withdrawal requested')
+const t3 = new Withdrawal(99.99, myAccount)
+console.log('Commit result:', t3.commit())
+
+console.log('Ending Account Balance: ', myAccount.balance)
+console.log("Back to parents house for free food!")
+
+console.log('Account Transaction History: ', myAccount.transactions)
